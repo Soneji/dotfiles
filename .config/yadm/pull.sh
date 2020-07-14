62ce8b987bd1c7212c4f43be7c9a5c68cc93b0e9
Dhaval Soneji <dhaval@soneji.xyz>
2020-07-14 11:20:00 +0100

    speed up automatic updates script

--- .config/yadm/pull.sh
+++ .config/yadm/pull.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
 function ting {
-    yadm pull --dry-run | grep -q -v 'Already up-to-date.' || 
+    UPSTREAM=${1:-'@{u}'}
+    LOCAL=$(yadm rev-parse @)
+    REMOTE=$(yadm rev-parse "$UPSTREAM")
+    BASE=$(yadm merge-base @ "$UPSTREAM")
+
+    if [ $LOCAL = $REMOTE ]; then
+        echo "Up-to-date"
+    elif [ $LOCAL = $BASE ]; then
+        echo "Need to pull"
         touch ~/.cache/dotfiles_update
+    elif [ $REMOTE = $BASE ]; then
+        echo "Need to push"
+    else
+        echo "Diverged"
+    fi
 }
 
 (&>/dev/null ting &)
@@ -11,7 +24,7 @@ if [ -f ~/.cache/dotfiles_update ]; then
     echo "Need to pull"
 
     echo 'Do you want to update now? (y/n)'
-    
+
     old_stty_cfg=$(stty -g)
     stty raw -echo ; answer=$(head -c 1) ; stty $old_stty_cfg # Careful playing with stty
     if echo "$answer" | grep -iq "^y" ;then
@@ -19,4 +32,5 @@ if [ -f ~/.cache/dotfiles_update ]; then
         yadm pull
         yadm stash apply
     fi
+    rm -f ~/.cache/dotfiles_update
 fi
