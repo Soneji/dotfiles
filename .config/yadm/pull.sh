3076b5b1ef353a657901b8a35a78d06f095fa9f1
Dhaval Soneji <dhaval@soneji.xyz>
2020-07-14 12:51:51 +0100

    update pull script

--- .config/yadm/pull.sh
+++ .config/yadm/pull.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 function ting {
+    yadm fetch
     UPSTREAM=${1:-'@{u}'}
     LOCAL=$(yadm rev-parse @)
     REMOTE=$(yadm rev-parse "$UPSTREAM")
@@ -15,15 +16,28 @@ function ting {
         echo "Need to push"
     else
         echo "Diverged"
+        touch ~/.cache/dotfiles_update_diverged
     fi
 }
 
 (&>/dev/null ting &)
 
 if [ -f ~/.cache/dotfiles_update ]; then
-    echo "Need to pull"
-
+    
+    echo "dotfiles: Need to pull"
     echo 'Do you want to update now? (y/n)'
+
+    old_stty_cfg=$(stty -g)
+    stty raw -echo ; answer=$(head -c 1) ; stty $old_stty_cfg # Careful playing with stty
+    if echo "$answer" | grep -iq "^y" ;then
+        yadm pull
+        rm -f ~/.cache/dotfiles_update
+    fi
+
+elif [ -f ~/.cache/dotfiles_update_diverged ]; then
+
+    echo "dotfiles: Need to pull but diverged"
+    echo 'Do you want to try automatic update now? (y/n)'
 
     old_stty_cfg=$(stty -g)
     stty raw -echo ; answer=$(head -c 1) ; stty $old_stty_cfg # Careful playing with stty
@@ -31,6 +45,8 @@ if [ -f ~/.cache/dotfiles_update ]; then
         yadm stash
         yadm pull
         yadm stash apply
+        rm -f ~/.cache/dotfiles_update_diverged
     fi
-    rm -f ~/.cache/dotfiles_update
+    echo 'Automatic update complete but may have errored see logs above'
 fi
+
